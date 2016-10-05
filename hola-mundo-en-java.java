e4485d41bc6cef93828d319298b5e59a0d5fa03d
Antonio Rodríguez Padilla <Antoniorp1980@gmail.com>
2016-10-05 09:05:51 +0200

    añadido colores lineas

--- hola-mundo-en-java.java
+++ hola-mundo-en-java.java
@@ -1,7 +1,9 @@
-/*ejercicio uno salida con mi nombre*/
+/*ejercicio uno salida con hola mundo*/
 /*prueba en git hub*/
 public class hola-mundo-en-java {
     public static void main (String [] args) {
-    System.out.println ("Antonio RP");
+      String color1 = "\033[32m";
+      String color1 = "\033[35m";
+    System.out.println (color1 + "hola" + color2 + "mundo");
   }
 }
